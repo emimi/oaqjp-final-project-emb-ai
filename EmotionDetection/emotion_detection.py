8c1f06bc21f1392bcbff6bf150deb0f6e1151ecb
emimi <emiharris@gmail.com>
2026-09-11 15:36:27 -0400

    Initial commit

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,13 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyze):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    jobj = { "raw_document": { "text": text_to_analyze } }
+    response = requests.post(url, json = jobj,  headers = headers)
+
+    emotions = json.loads(response.text)["emotionPredictions"][0]["emotion"]
+    emotions["dominate_emotion"] = max(emotions, key=emotions.get)
+
+    return emotions
